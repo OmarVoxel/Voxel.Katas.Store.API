472856167e98a7f82798d6631e56b0f960555522
Omar Izquierdo <oizquierdo@voxelgroup.net>
2022-11-04 14:44:16 +0100

    chore: add swagger

--- Voxel.Katas.Store.API/Program.cs
+++ Voxel.Katas.Store.API/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseSwaggerUI();
+app.UseSwagger(x => x.SerializeAsV2 = true);
+app.MapControllers();
 app.Run();
